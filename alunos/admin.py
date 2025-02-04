fb8aeb8ca15ec73e1f2b80a8292561f90da514fb
Lucas-EstudanteIFC <alunolucasmartins62@gmail.com>
2025-02-04 15:17:45 -0300

    continuacao

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Estado
+from .models import Cidade
+from .models import Pessoa
 
 admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Pessoa)
--- alunos/models.py
+++ alunos/models.py
@@ -2,7 +2,24 @@ from django.db import models
 
 class Estado(models.Model): 
     nome = models.CharField(max_length=50)
-    sigle = models.CharField(max_length=2)
+    sigla = models.CharField(max_length=2)
     
+    def __str__ (self):
+        return f"{self.nome} ({self.sigla})"
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def __str__ (self):
+        return f"{self.nome} ({self.estado.sigla})"
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=40)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    dataNascimento= models.DateField
+    cidade=models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
     def __str__ (self):
         return self.nome
